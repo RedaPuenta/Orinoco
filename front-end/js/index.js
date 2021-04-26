91f892e4ffe355526552f41e1c8592906a379b77
RedaPuenta <rida.hadj@hotmail.com>
2021-04-26 06:06:04 +0200

    Refonte --> code JS

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,30 +1,17 @@
 // Requête HHTP pour demander à l'API, la liste d'article (méthode GET)
 fetch("http://localhost:3000/api/cameras")
 
-// 1ère Promesse --> On capture la réponse de l'API et on les transforment en objet JavaScript
+// 1ère Promesse --> On capture la liste de produit en format JSON et on la transforme en objet JavaScript
 .then(function(reponse){
     return reponse.json()
 })
 
-// 2ème Promesse --> On capture le nouveau objet contenant les données de l'API
+// 2ème Promesse --> On capture le nouveau objet contenant la liste de produit
 .then(function(reponse){
 
-    // BOUCLE --> Celle-ci va nous servir à distribuer les données du nouvelle l'objet
-    for (let i = 0; i < reponse.length; i++) {
+    // On exécute la fonction qui permet d'afficher la liste de produit dans la page d'accueil
+    displayProductInHomePage(reponse)
 
-        // Ici, on attrape le <template> qu'on a créer dans la page "index.html"
-        const templateProductList = document.getElementById("template-product-list")
-        // Ici, on clone le <template>
-        const templateProductListClone = document.importNode(templateProductList.content, true)
-        // Ici, dans le <template> cloné, on distribut aux éléments appropriés, les valeurs du nouvelle objet (id, image, nom, prix) 
-        templateProductListClone.getElementById("collection-direction").setAttribute("href", "pages/product.html?id=" + reponse[i]._id)
-        templateProductListClone.getElementById("image-product").setAttribute("src", reponse[i].imageUrl)
-        templateProductListClone.getElementById("image-product").setAttribute("alt", "image de la caméra " + reponse[i].name)
-        templateProductListClone.getElementById("name-product").textContent = reponse[i].name
-        templateProductListClone.getElementById("price-product").textContent = (reponse[i].price/1000).toFixed(2) + "€"
-        // Ici, on ajoute le <template> personnalisé (nouvelle article) dans la page "index.html"
-        document.getElementById("collection__group__camera").appendChild(templateProductListClone)
-    }
 })
 
 // ERROR CATCH --> En cas d'erreur liés aux promesses, on affiche l'erreur dans la console
@@ -32,10 +19,29 @@ fetch("http://localhost:3000/api/cameras")
     console.log(error);
 })
 
-// Si il y a au moins 1 objet dans le Session Storage ...
-if(sessionStorage.length >= 1 ){
+// FONCTION --> Permet d'afficher la liste de produit dans la page d'accueil
+function displayProductInHomePage(productList) {
     
-    // On exécute la fonction qui affiche le nombre totale d'article qui ont été ajouté (dans l'icone panier)
-    displayNumberInPanier()
+    // BOUCLE --> Celle-ci va nous servir à distribuer les valeurs de la liste de produit
+    for (let i = 0; i < productList.length; i++) {
 
+        // On attrape le <template> qu'on a créer dans la page "index.html"
+        const templateProductList = document.getElementById("template-product-list")
+        // On clone ce <template>
+        const templateProductListClone = document.importNode(templateProductList.content, true)
+        // Dans le <template> cloné, on distribut aux éléments appropriés, les valeurs de la liste de produit (id, image, nom, prix) 
+        templateProductListClone.getElementById("collection-direction").setAttribute("href", "pages/product.html?id=" + productList[i]._id)
+        templateProductListClone.getElementById("image-product").setAttribute("src", productList[i].imageUrl)
+        templateProductListClone.getElementById("image-product").setAttribute("alt", "image de la caméra " + productList[i].name)
+        templateProductListClone.getElementById("name-product").textContent = productList[i].name
+        templateProductListClone.getElementById("price-product").textContent = priceInEuro(productList[i].price)
+        // Et on ajoute le <template> personnalisé (nouveau produit) dans la page "index.html"
+        document.getElementById("collection__group__camera").appendChild(templateProductListClone)
+    }
 }
+
+// On exécute la fonction qui permet d'afficher, au-dessus de l'icone panier, le nombre totale de produit qui ont été ajouté
+displayNumberAbovePanier()
+
+// On exécute la fonction qui permet de bloquer l'accès au panier quand celui-ci est vide
+stopAccessPanierEmpty()
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,40 +1,24 @@
-// Ici, on préviens JavaScript qu'on va chercher dans l'URL un paramètre
+// Ici, on récupère dans l'URL, l'Id du produit ciblé
 let params = (new URL(document.location)).searchParams
-
-// Ici, on récupère la valeur du paramètre recherché (ID du produit)
 let IdSearch = params.get("id")
 
-// Requête HHTP pour demander à l'API, les données de l'article en question (méthode GET)
+// Requête HHTP pour demander à l'API, les données du produit en question (méthode GET)
 fetch(`http://localhost:3000/api/cameras/${IdSearch}`)
 
-// 1ère Promesse --> On capture la réponse de l'API et on les transforment en objet JavaScript
+// 1ère Promesse --> On capture le produit en format JSON et on le transforme en objet JavaScript
 .then(function(reponse){
     return reponse.json()
 })
 
-// 2ème Promesse --> On capture le nouveau objet contenant les données de l'API
+// 2ème Promesse --> On capture le nouveau objet contenant le produit
 .then(function(reponse){
-    console.log(reponse)
 
-    // Ici, dans <article> de la page "product.html", on distribut à ses enfants, les valeurs du nouvelle objet (id, image, nom, prix) 
-    document.getElementById("image-product-focus").setAttribute("src", reponse.imageUrl)
-    document.getElementById("name-product-focus").innerHTML = reponse.name
-    document.getElementById("price-product-focus").innerHTML = (reponse.price/1000).toFixed(2) + " €"
-    document.getElementById("description-product-focus").innerHTML = reponse.description
+    // On exécute la fonction qui permet d'afficher le ou les produits ajoutés dans la page panier
+    displayProductInProductPage(reponse)
 
-    // BOUCLE --> Celle-ci va nous servir à distribuer les données "lenses" du nouvelle l'objet
-    for (let i = 0; i < reponse.lenses.length; i++) {
-        
-        // Ici, créer un nouveau élément <option>
-        let optionNew = document.createElement("option")
-        optionNew.setAttribute("value", reponse.lenses[i])
-        document.getElementById("product-lenses").appendChild(optionNew)
-        // Ici, dans ce nouveau élément <option>, on lui donne une valeur de "lenses"
-        optionNew.innerHTML = reponse.lenses[i]
-    }
-
-    // On exécute la fonction qui va stocker les ajouts au panier
-    saveArticleAdd(reponse)
+    // On exécute la fonction qui permet d'ajouter un ou plusieurs dans le SESSION STORAGE (TRIÉ)
+    addProductInSessionStorage(reponse)
+    
 })
 
 // ERROR CATCH --> En cas d'erreur liés aux promesses, on affiche l'erreur dans la console
@@ -42,15 +26,35 @@ fetch(`http://localhost:3000/api/cameras/${IdSearch}`)
     console.log(error);
 })
 
-// FONCTION --> Celle-ci va nous servir à stocker les ajouts au panier
-function saveArticleAdd(reponse){
+// FONCTION --> Permet d'afficher le produit ciblé dans la page produit
+function displayProductInProductPage(productCible){
 
-    // Ici, on attrape les balises <input> qui se chargent de la personnalisation du produit (lenses, quantité)
+    // Ici, dans la balise <article> de la page "product.html", on distribut à ses enfants, les valeurs du produit (id, image, nom, prix) 
+    document.getElementById("image-product-focus").setAttribute("src", productCible.imageUrl)
+    document.getElementById("name-product-focus").innerHTML = productCible.name
+    document.getElementById("price-product-focus").innerHTML = priceInEuro(productCible.price)
+    document.getElementById("description-product-focus").innerHTML = productCible.description
+
+    // BOUCLE --> Celle-ci va nous servir à distribuer les données relatives aux "lentilles" du produit
+    for (let i = 0; i < productCible.lenses.length; i++) {
+        
+        // On créer une nouvelle balise <option>, on lui donne une valeur de "lentille" et on l'insère dans son parent <select>
+        let optionNew = document.createElement("option")
+        optionNew.setAttribute("value", productCible.lenses[i])
+        optionNew.innerHTML = productCible.lenses[i]
+        document.getElementById("product-lenses").appendChild(optionNew)
+    }
+}
+
+// FONCTION --> Permet d'ajouter un ou plusieurs dans le SESSION STORAGE (TRIÉ)
+function addProductInSessionStorage(productCible){
+
+    // Ici, on attrape les balises <input> qui se chargent de la personnalisation du produit (lentilles, quantité)
     let lensesOptional = document.getElementById("product-lenses")
     let quantity = document.getElementById("product-quantity")
     // Ici, on attrape la balise <button> qui se charge de l'ajout au panier
     let buttonAddPanier = document.getElementById("focus-product-shop")
-    // Ici, on créer un tableau vide qui va accueillir les nouveaux articles à ajouter
+    // Ici, on créer un tableau vide qui va accueillir les nouveaux produits à ajouter
     let articleAddFinally = []
 
     // EVENEMENT --> Ecoute le clic sur le bouton "Ajouter au panier"
@@ -59,12 +63,12 @@ function saveArticleAdd(reponse){
         // On retire le comportement par défaut du bouton
         event.preventDefault()
         
-        // Ici, on créer un objet qui va contenir les informations sur l'article 
+        // On créer un objet qui va contenir les informations sur le produit 
         let addNewArticle = {
-            productId: reponse._id,
-            productName: reponse.name,
-            productImage: reponse.imageUrl,
-            productPrice: reponse.price,
+            productId: productCible._id,
+            productName: productCible.name,
+            productImage: productCible.imageUrl,
+            productPrice: productCible.price,
             productLenses: lensesOptional.value,
             productQuantity: quantity.textContent
         }
@@ -72,81 +76,102 @@ function saveArticleAdd(reponse){
         // Si il n'y a pas de KEY enrengistré dans le SESSION STORAGE ...
         if (sessionStorage.getItem('TotalArticleAdd') === null) {
 
-            // On ajoute le nouveau article dans la liste
+            // On ajoute le nouveau produit dans la liste
             articleAddFinally.push(addNewArticle)
-            // On envoie cette liste dans le SESSION STORAGE
+            // Et on envoie cette liste dans le SESSION STORAGE
             sessionStorage.setItem("TotalArticleAdd", JSON.stringify(articleAddFinally))
-        
         
         // Sinon, si il y a une déjà une KEY enrengistré dans le SESSION STORAGE ... 
         } else {
 
-            // On récupère la liste d'article du SESSION STORAGE
+            // On récupère la liste de produit du SESSION STORAGE
             articleAddFinallyRecup = JSON.parse(sessionStorage.getItem('TotalArticleAdd'))
 
-            // BOUCLE --> Celle-ci va nous servir à parcourir la liste qui a été récupéré dans le SESSION STORAGE
+            // BOUCLE --> Celle-ci va nous servir à parcourir la liste de produit qui a été récupéré dans le SESSION STORAGE
             for (let i = 0; i < articleAddFinallyRecup.length; i++) {
                 
-                // Si dans la liste récupéré, il y a un article qui a le même "Id" et le même "Lenses" que celui qui veut être ajouté ...
+                // Si dans la liste récupéré, il y a un produit qui a le même "Id" et le même "Lenses" que celui qui veut être ajouté ...
                 if(articleAddFinallyRecup[i].productId == addNewArticle.productId && articleAddFinallyRecup[i].productLenses == addNewArticle.productLenses ){
+                    
+                    // On indique dans une 1ère variable qu'on a trouvé un produit similaire
                     var find = true
+                    // On indique dans une 2ème variable l'index où se produit a été trouvé
                     var findIndex = i
+                    // Et sors de la boucle (car ça sert à rien de continuer à chercher comme y aura pas d'autres produit similaire)
                     break
                 }
             }
 
-            // Si la boucle ci-dessus a trouvé un article similaire à celui l'article qui veut être ajouté ...
+            // Si la boucle ci-dessus a trouvé un produit similaire à celui du produit qui veut être ajouté ...
             if (find === true) {
 
-                // On rajoute seulement à la quantité de l'article similaire, la quantité de l'article qui veut être ajouté
+                // On rajoute seulement à la quantité du produit similaire, la quantité du produit qui veut être ajouté
                 articleAddFinallyRecup[findIndex].productQuantity = parseInt(articleAddFinallyRecup[findIndex].productQuantity) + parseInt(addNewArticle.productQuantity)
-                // On remplace la liste du SESSION STORAGE par la liste récupéré qu'on a mis à jour
+                // Et on remplace la liste du SESSION STORAGE par la liste récupéré qu'on a mis à jour
                 sessionStorage.setItem("TotalArticleAdd", JSON.stringify(articleAddFinallyRecup))
             
-            // Sinon, si la boucle ci-dessus n'a pas trouvé un article similaire à celui qui va être ajouté ...  
+            // Sinon, si la boucle ci-dessus n'a pas trouvé un produit similaire à celui qui veut être ajouté ...  
             } else {
 
-                // On ajoute le nouveau article dans la liste récupéré
+                // On ajoute le nouveau produit dans la liste récupéré
                 articleAddFinallyRecup.push(addNewArticle)
                 // On remplace la liste du SESSION STORAGE par la liste récupéré qu'on a mis à jour
                 sessionStorage.setItem("TotalArticleAdd", JSON.stringify(articleAddFinallyRecup))
             }
         }
 
-        // On exécute la fonction qui affiche le nombre totale d'article qui ont été ajouté (dans l'icone panier)
-        displayNumberInPanier()
+        // On exécute la fonction qui permet d'afficher, au-dessus de l'icone panier, le nombre totale de produit qui ont été ajouté
+        displayNumberAbovePanier()
     })
 }
 
-// Si il y a au moins 1 objet dans le Session Storage ...
-if(sessionStorage.length >= 1 ){
-    
-    // On exécute la fonction qui affiche le nombre totale d'article qui ont été ajouté (dans l'icone panier)
-    displayNumberInPanier()
+// FONCTION --> Permet de faire fonctionner les bouttons de quantité
+function btnQuantityBase(valueDefault){
+
+    let btnQuantityList = document.querySelectorAll(".gen-btn-quantity")
+ 
+     // Ici, on attrape les balises <button> qui se chargent de contrôler la quantité du produit
+     let buttonMoins = document.querySelectorAll("#button-add-less")
+     let buttonPlus = document.querySelectorAll("#button-add-more")
+     
+     // Ici, on attrape la balise <p> qui se charge de stocker et d'afficher la quantité du produit
+     let resultQuantity = document.querySelectorAll("#product-quantity")
+ 
+     for (let i = 0; i < btnQuantityList.length; i++) {
+         
+         // Ici, on ajoute dans la balise <p>, la quantité par défaut
+         resultQuantity[i].textContent = valueDefault
+     }
+ 
+     for (let i = 0; i < btnQuantityList.length; i++) {
+         
+         // EVENEMENT --> Ecoute le clic sur le bouton + du contrôle de quantité
+         buttonPlus[i].addEventListener("click", function(){
+ 
+             // On ajoute 1 à la quantité
+             resultQuantity[i].textContent = parseInt(resultQuantity[i].textContent) + 1
+         })
+ 
+         // EVENEMENT --> Ecoute le clic sur le bouton - du contrôle de quantité
+         buttonMoins[i].addEventListener("click", function(){
+ 
+             // Si la quantité est strictement supérieur à 1 ... (SÉCURITÉ)
+             if(parseInt(resultQuantity[i].textContent) > 1){
+ 
+                 // On enlève 1 à la quantité
+                 resultQuantity[i].textContent = parseInt(resultQuantity[i].textContent) - 1
+             }
+         })
+     }
+ 
+ 
 }
 
-// Ici, on attrape les balises <button> qui se chargent de contrôler la quantité du produit
-let buttonMoins = document.getElementById("button-add-less")
-let buttonPlus = document.getElementById("button-add-more")
-// Ici, on attrape la balise <p> qui se charge de stocker et d'afficher la quantité du produit
-let resultQuantity = document.getElementById("product-quantity")
-// Ici, on ajoute dans la balise <p>, la quantité par défaut
-resultQuantity.textContent = 1
+// On éxécute la fonction qui permet de faire fonctionner les bouttons de quantité
+btnQuantityBase(1)
 
-// EVENEMENT --> Ecoute le clic sur le bouton + du contrôle de quantité
-buttonPlus.addEventListener("click", function(){
+// On exécute la fonction qui permet d'afficher, au-dessus de l'icone panier, le nombre totale de produit qui ont été ajouté
+displayNumberAbovePanier()
 
-    // On ajoute 1 à la quantité
-    resultQuantity.textContent = parseInt(resultQuantity.textContent) + 1
-})
-
-// EVENEMENT --> Ecoute le clic sur le bouton - du contrôle de quantité
-buttonMoins.addEventListener("click", function(){
-
-    // Si la quantité est strictement supérieur à 1 ... (SÉCURITÉ)
-    if(parseInt(resultQuantity.textContent) > 1){
-
-        // On enlève 1 à la quantité
-        resultQuantity.textContent = parseInt(resultQuantity.textContent) - 1
-    }
-})
+// On exécute la fonction qui permet de bloquer l'accès au panier quand celui-ci est vide
+stopAccessPanierEmpty()

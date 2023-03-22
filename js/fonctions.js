298e6871669f60cf6f2b55f10f3ab9346c2ae9f6
Rida Benkou <78867127+RedaPuenta@users.noreply.github.com>
2023-03-22 22:52:55 +0100

    Update fonctions.js

--- js/fonctions.js
+++ js/fonctions.js
@@ -1,5 +1,5 @@
 function base_uri(){
-    return "https://reda-serveur.com/api/orinoco"
+    return "https://ridabenkou.com/api/orinoco"
 }
 
 // FONCTION --> Permet de convertir les prix de l'API en EURO
